75fe28bc8fd3b1638b189ffc2520a9488886dd3b
Rama Olivera Fedi <rramiro.o@hotmail.com>
2017-05-18 11:24:54 -0300

    Add summary view

--- src/sell/Success.js
+++ src/sell/Success.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import Colors from '../constants/Colors';
 import { connect } from 'react-redux';
-import TicketField from './TicketField'
-import moment from 'moment';
 import { Link } from 'react-router-dom';
+import BoardingPass from './BoardingPass';
 
 class Success extends Component {
   constructor(props) {
@@ -79,26 +78,7 @@ class Success extends Component {
           <h2 style={styles.h2}>Has adquirido {adults + children + infants} { adults + children + infants === 1 ? 'pasaje' : 'pasajes'}  con destino a {this.state.flight.arrivalCity.name}. <br/> Esperamos que disfrutes tu viaje.</h2>
         </div>
         
-        <div style={styles.ticketContainer}>
-          <div style={styles.tripContainer}>
-            <div style={styles.ticketHeader}><span>{ this.state.flight.airline.name }</span> <span>VUELO #{this.state.flight.id}</span></div>
-            <TicketField title="Desde">{ this.state.flight.departureCity.name } ({ this.state.flight.departureAirport.id.toUpperCase()})</TicketField>
-            <TicketField title="Partida" style={{ width: 'auto' }}>{ moment(this.state.flight.departureTime).format('DD/MM/YYYY hh:mm')} ({this.state.flight.departureAirport.timeZone}) </TicketField>
-            <TicketField title="Hasta">{ this.state.flight.arrivalCity.name } ({ this.state.flight.arrivalAirport.id.toUpperCase()})</TicketField>
-            <TicketField title="Llegada" style={{ width: 'auto' }}>{ moment(this.state.flight.arrivalTime).format('DD/MM/YYYY hh:mm')} ({this.state.flight.arrivalAirport.timeZone})</TicketField>
-          </div>
-
-          <div style={styles.billContainer}>
-            <div style={{...styles.ticketHeader, justifyContent: 'center'}}>TICKET</div>
-            <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '80%', margin: '0 auto', height: 'calc(100% - 60px)', fontSize: 19, textAlign: 'left'}}>
-              { adults ? <div style={styles.billItemContainer}><span>{ adults } { adults == 1 ? 'Adulto' : 'Adultos' }:</span><span>{adults} x U$D { this.state.flight.rawPrice.adults.base_fare }</span></div> : null }
-              { children ? <div style={styles.billItemContainer}><span>{ children } { children == 1 ? 'Niño' : 'Niños' }:</span><span>{children} x U$D { this.state.flight.rawPrice.children.base_fare }</span></div> : null }
-              { infants ? <div style={styles.billItemContainer}><span>{ infants } { infants == 1 ? 'Infante' : 'Infantes'}:</span><span>{infants} x U$D { this.state.flight.rawPrice.infants.base_fare }</span></div> : null }
-              <div style={{...styles.billItemContainer, fontSize: 14, margin: '5px 0'}}><span>Impuestos y cargos:</span><span>U$D { this.state.flight.rawPrice.total.charges + this.state.flight.rawPrice.total.taxes }</span></div>
-              <div style={{ width: '100%', borderTop: `3px solid ${Colors.celest}`, color: Colors.blue, margin: '0 auto', marginTop: 5, paddingTop: 10, textAlign: 'center', fontWeight: 900, fontSize: 22}}>TOTAL: U$D {this.state.flight.rawPrice.total.total }</div>
-            </div>
-          </div>
-        </div>
+        <BoardingPass style={{ marginTop: 20 }} flight={this.state.flight} />
 
         <Link to="/">
           <button style={styles.button}>DESCUBRÍ NUEVOS DESTINOS</button>        
@@ -134,38 +114,6 @@ const styles = {
     fontSize: 30,
     fontWeight: 400,
     marginTop: 20, 
-  }, ticketContainer: {
-    display: 'flex',
-    justifyContent: 'flex-start',
-    marginTop: 20,
-    width: 900,
-    background: Colors.gray,
-    border: '1px solid grey',
-  }, ticketHeader: {
-    width: '100%',
-    boxSizing: 'border-box',
-    fontSize: 25,
-    fontWeight: 900,
-    color: Colors.blue,
-    height: 60,
-    background: Colors.celest,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: '0 20px',
-  }, tripContainer: {
-    width: '70%',
-    textAlign: 'left',
-    boxSizing: 'border-box',
-    display: 'inline-block',
-  }, billContainer: {
-    width: '30%',
-    boxSizing: 'border-box',
-    display: 'inline-block',
-    borderLeft: '1px dashed grey',
-  }, billItemContainer : {
-    display: 'flex',
-    justifyContent: 'space-between',
   }, button: {
     backgroundColor: Colors.pink,
     border: '1px solid red',
