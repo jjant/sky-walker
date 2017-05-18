c519c2c315ba6578f8470452ece42d1a0d254676
Rama Olivera Fedi <rramiro.o@hotmail.com>
2017-05-18 14:50:46 -0300

    Improve slightly 404 page

--- src/not_found/NotFound.js
+++ src/not_found/NotFound.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import Colors from '../constants/Colors';
 
 const NotFound = () => (
   <div>
-    <h1>
-      404 <small>Not Found, gato</small>
-    </h1>
+    <h1 style={{ color: Colors.pink, margin: 0, marginTop: 60, position: 'relative', fontSize: 50 }}>No hay nada que ver aquí</h1>
+    <h3>Probá de escribir otra dirección URL, o volver a la <a href="/">Página principal</a></h3>
   </div>
 );
 
