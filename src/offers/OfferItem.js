76399198cbfbfacaf5dcf0b7c650b24518d790b6
Rama Olivera Fedi <rramiro.o@hotmail.com>
2017-05-19 19:07:05 -0300

    Improve offer items

--- src/offers/OfferItem.js
+++ src/offers/OfferItem.js
@@ -5,15 +5,31 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const OfferItem = ({ offer, image }) => {
-  const fullname = `${offer.city.name} ${offer.country.id === 'AR' ? ', Argentina' : ""}`;
-  return (
-    <div style={{...styles.container, height: getRandomInt(225, 275), width: 280 }}>
-      <img src={image} style={styles.image}/>
-      <h1 style={styles.text}>{ offer.city.name }{ offer.country.id === 'AR' ? ', Argentina' : "" }</h1>
-      <h2 style={styles.text}> A solo U$D{offer.price}</h2>
-    </div>
-  );
+export default class OfferItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { imageHeight: 0};
+  }
+
+  _onLoad = (e) => {
+    console.log(e.target)
+    this.setState({ imageHeight: e.target.offsetHeight });
+  }
+
+  render() {
+    const offer = this.props.offer;
+    const image = this.props.image;
+    const fullname = `${offer.city.name} ${offer.country.id === 'AR' ? ', Argentina' : ""}`;
+
+    return (
+      <div onMouseEnter={this._onMouseOver} onMouseLeave={this._onMouseOut} style={{...styles.container, height: this.state.imageHeight, width: 280 }}>
+        <img className='offer-item' src={image} onLoad={this._onLoad} style={styles.image}/>
+        <div style={styles.overlay}></div>
+        <h1 style={styles.text}>{ offer.city.name }{ offer.country.id === 'AR' ? ', Argentina' : "" }</h1>
+        <h2 style={styles.price}> A solo U$D{offer.price}</h2>
+      </div>
+    );
+  }
 }
 
 const styles = {
@@ -25,12 +41,27 @@ const styles = {
   text: {
     position: 'relative',
     color: 'white',
+    fontSize: 15,
   },
   image: {
     width: '100%',
     position: 'absolute',
     left: '0',
   },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: '100%',
+    width: '100%',
+    background: 'rgba(0,0,0,.5)',
+  },
+  price: {
+    fontWeight: 400,
+    position: 'absolute',
+    bottom: -10,
+    right: 10,
+    color: 'white',
+    fontSize: 18,
+  }
 };
-
-export default OfferItem;
