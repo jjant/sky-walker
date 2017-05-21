c7f22b4924a05f33ba3f32b4536facabf0a07198
Rama Olivera Fedi <rramiro.o@hotmail.com>
2017-05-20 22:43:09 -0300

    Working internet check

--- src/lib/InternetCheck.js
+++ src/lib/InternetCheck.js
@@ -4,14 +4,13 @@ import Alert from 'react-s-alert';
 export default class InternetCheck extends Component {
   componentWillMount() {
     const msj = 'Parece que no tiene conexión a internet. Compruebe su conexión y recuperela si quiere seguir con su compra.';
-    /*setInterval(async () => {
+    setInterval(async () => {
       try {
-        const res = await fetch('https://www.google.com.ar/?gws_rd=ssl');
-        if (!res.ok) Alert.error(msj);
+        const res = await fetch('https://www.google.com.ar/?gws_rd=ssl', { mode: 'no-cors' });
       } catch (e) {
         Alert.error(msj);
       }
-    }, 1000 * 60 * 2)*/
+    }, 1000 * 30)
   }
 
   render() {
